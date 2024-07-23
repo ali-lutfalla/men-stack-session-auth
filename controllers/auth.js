da7eaa0adfb42615bfcbaa59363b1077a86ccfcd
ali-lutfalla <ali,latfalla@gmail.com>
2024-07-23 11:02:42 +0300

    add the sign up form yemplate and aded the sign-up route

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/sign-up',(req, res) => {
-    res.render("sign-up.ejs");
+    res.render("auth/sign-up.ejs");
 })
 
 module.exports = router;
